975ddcdfb7e55654d3af5f33df2f477781f7a6c6
Daiana Gavrylenko <g-dayana@ukr.net>
2023-12-09 20:00:49 +0100

    Created Observer and Explorer Bees

---
+++ GraphABC/GraphABC.Implementation/ExplorerBee.cs
@@ -0,0 +1,40 @@
+﻿namespace GraphABC.Implementation;
+
+public class ExplorerBee
+{
+    public ExplorerBee(Network net)
+    {
+        Network = net;
+        AlreadyChosen = new HashSet<Node>(net.Nodes.Count);
+        ChosenNodeId = -1;
+    }
+
+    public Network Network { get; }
+    public int ChosenNodeId { get; set; }
+    public HashSet<Node> AlreadyChosen { get; }
+
+    public Node PickNode()
+    {
+        var node = Network.Nodes
+            .Where(n => !AlreadyChosen.Contains(n))
+            .OrderByDescending(n => n.Connectivity)
+            .FirstOrDefault()!;
+
+        ChosenNodeId = node.Identifier;
+        AlreadyChosen.Add(node);
+        return node;
+    }
+
+    public double EvaluateNodeValue(Node node, IEnumerable<Node> chosenNodes, int observers)
+    {
+        var totalConnectivity = chosenNodes.Sum(n => n.Connectivity);
+        return observers * ((double)node.Connectivity / totalConnectivity);
+    }
+
+    public static IEnumerable<Node> SelectTopNodes(Network net, int number)
+    {
+        return net.Nodes
+            .OrderByDescending(n => n.Connectivity)
+            .Take(number);
+    }
+}
